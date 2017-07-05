131670b781ac9583ec2bc87f5bab0d4bb3790199
Hulijun <17764592770@163.com>
2017-07-05 14:47:00 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by hlj on 2017/7/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
